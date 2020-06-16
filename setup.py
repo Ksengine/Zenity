6f3a40da351f8cbc1fc35c74c556b138c386313a
Ksengine <63494497+Ksengine@users.noreply.github.com>
2020-06-16 11:28:13 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,22 +4,17 @@
 Zenity setup script.
 """
 __doc__="""zenity - display dialogs with python
-
-SYNOPSIS
-       zenity [options]
-
 DESCRIPTION
-zenity  is a Python library that will display GTK+ dialogs using zanity
- tool, and return (eitherin the return code, or on standard output) the
- users input. This allows you to present information, and ask for infor
+zenity  is a Python library that will display GTK+ dialogs using zanity 
+tool, and return (eitherin the return code, or on standard output) the 
+users input. This allows you to present information, and ask for infor
 mation from the user.
 
 For example, zenity.show(zenity.question) will return either 0, 1 or 5,  depending
 on  whether  the  user  pressed OK, Cancel or timeout has been reached.
 zenity.show(zenity.entry) will output on standard output what the user typed  into
 the text entry field.
-
-       Comprehensive documentation is coming soon.
+Comprehensive documentation is coming soon.
 
 ENVIRONMENT
 Normally, zenity detects the window from which it was launched
