65031a332ff06f844ab43cd9d2289765a46e65f8
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-01-06 11:53:57 +0000

    kwargs parser fix
    used `repr` method to convert keyword argument to `str`(string)

--- zenity.py
+++ zenity.py
@@ -104,9 +104,9 @@ def show(*args,**kwargs):
             w=True
             del kwargs["writeable"]
         for kwarg in kwargs:
-            flags_list.append("--"+kwarg+"=\""+str(kwargs[kwarg])+"\"")
+            flags_list.append("--"+kwarg+"="+repr(kwargs[kwarg]))
         for arg in args:
-            flags_list.append("--"+str(arg))
+            flags_list.append("--"+repr(arg))
         if w:
             return _message(flags_list,writeable=True)
         return _message(flags_list)
